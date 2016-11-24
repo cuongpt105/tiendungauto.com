52debbcf9e74d3b27688759681ee07f8a907aa8c
cuongpt105 <cuongpt105@gmail.com>
2016-11-24 23:56:31 +0700

    test

--- dao/file-system.dao.js
+++ dao/file-system.dao.js
@@ -1,4 +1,4 @@
-var HandleException = require('../util/HandleException');
+var HandleException = require('../util/handleException');
 
 var FileSystemEntity = require('../entity/file-system.entity');
 
